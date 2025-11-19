c92e8c6a2d779504b211df1f801bbf7387ff7dd5
Stasshe <egnm9stasshe@gmail.com>
2025-11-19 22:53:02 +0900

    下限 40

--- src/app/Chart.tsx
+++ src/app/Chart.tsx
@@ -11,7 +11,7 @@ export default function Chart({ data, subjects }: { data: DataPoint[]; subjects:
       <ResponsiveContainer width="100%" height="100%">
         <LineChart data={data} margin={{ top: 10, right: 20, left: 0, bottom: 0 }}>
           <XAxis dataKey="date" />
-          <YAxis domain={[0, 100]} />
+          <YAxis domain={[40, 100]} />
           <Tooltip />
           <Legend />
           {subjects.map((s, i) => (
